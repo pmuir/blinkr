c2bdd0a46a286ab6e3c1eb2af6c6f42e6ae7406d
Pete Muir <pmuir@bleepbleep.org.uk>
2014-07-24 17:56:02 +0100

    Fix descriptive text

--- lib/blinkr/extensions/empty_a_href.rb
+++ lib/blinkr/extensions/empty_a_href.rb
@@ -9,7 +9,7 @@ module Blinkr
       def collect page
         page.body.css('a[href]').each do |a|
           if a['href'].empty?
-            page.errors << OpenStruct.new({ :type => 'empty_a_href',  :title => %Q{empty <a href="" /> (line #{a.line})}, :message => %Q{empty <a href="" />}, :snippet => a.to_s, :icon => 'fa-info' })
+            page.errors << OpenStruct.new({ :type => 'empty_a_href',  :title => %Q{empty <a href=""> (line #{a.line})}, :message => %Q{empty <a href="">}, :snippet => a.to_s, :icon => 'fa-info' })
           end
         end
       end
