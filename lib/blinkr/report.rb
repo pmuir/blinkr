143a3d92c38c9dbde0cc72fbd0d403f94aded6e6
LightGuard <lightguard.jp@gmail.com>
2015-09-10 14:55:28 -0600

    Enhancing the report, adding pagination

--- lib/blinkr/report.rb
+++ lib/blinkr/report.rb
@@ -22,21 +22,32 @@ module Blinkr
       @context.severity = {}
       @context.category = {}
       @context.type = {}
-      @context.pages.each do |_, page|
+      @context.pages.each do |url, page|
+        page.url = url
         page.max_severity = ::Blinkr::SEVERITY.first # :success
+        page.severities = []
+        page.categories = []
+        page.types = []
         page.errors.each do |error|
           raise "#{error.severity} not a valid severity. Must be one of #{::Blinkr::SEVERITY.join(',')}" unless ::Blinkr::SEVERITY.include? error.severity
           raise "#{error.category} must be specified." if error.category.nil?
           @context.total += 1
           @context.severity[error.severity] ||= OpenStruct.new({:count => 0})
           @context.severity[error.severity].count += 1
+          page.severities << error.severity
           page.max_severity = error.severity if ::Blinkr::SEVERITY.index(error.severity) > ::Blinkr::SEVERITY.index(page.max_severity)
           @context.category[error.category] ||= OpenStruct.new({:count => 0})
           @context.category[error.category].count += 1
+          page.categories << error.category
           @context.type[error.type] ||= OpenStruct.new({:count => 0})
           @context.type[error.type].count += 1
+          page.types << error.type
         end
+        page.severities.uniq!
+        page.categories.uniq!
+        page.types.uniq!
       end
+      @context.pages = @context.pages.values
       File.open(@config.report, 'w') do |file|
         file.write(Slim::Template.new(TMPL).render(OpenStruct.new({:blinkr => @context, :engine => @engine,
                                                                    :errors => @context.to_json})))
