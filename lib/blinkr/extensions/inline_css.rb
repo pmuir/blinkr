6336faec9302937dc7c590e8dd2ab89212716d2b
LightGuard <lightguard.jp@gmail.com>
2015-09-09 23:14:44 -0600

    Proper amount of a snippet
    We now have the right level of context for a snippet in the inline_css
    check

--- lib/blinkr/extensions/inline_css.rb
+++ lib/blinkr/extensions/inline_css.rb
@@ -10,18 +10,20 @@ module Blinkr
 
       def collect page
         page.body.css('[style]').each do |elm|
+          elm_clone = Nokogiri.make(elm.to_s)
+          elm_clone.inner_html = ""
           if elm['style'] == ""
             page.errors << Blinkr::Error.new({:severity => 'info', :category => 'HTML Compatibility/Correctness',
                                               :type => 'style attribute is empty',
                                               :title => %Q{"#{elm['style']}" (line #{elm.line})},
-                                              :message => 'style attribute is empty', :snippet => elm.to_s,
+                                              :message => 'style attribute is empty', :snippet => elm_clone.to_s,
                                               :icon => 'fa-info'})
           else
             page.errors << Blinkr::Error.new({:severity => 'info',
                                               :category => 'HTML Compatibility/Correctness',
                                               :type => 'Inline CSS detected',
                                               :title => %Q{"#{elm['style']}" (line #{elm.line})},
-                                              :message => 'inline style', :snippet => elm.to_s,
+                                              :message => 'inline style', :snippet => elm_clone.to_s,
                                               :icon => 'fa-info'})
           end
         end
